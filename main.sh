8c68042fee5df92b7f420fdfe84f2daea522cbfa
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-12-05 09:02:00 +0800

    Update

--- main.sh
+++ main.sh
@@ -2,12 +2,9 @@ FileName=$(basename reader*.jar)
 tag2=$(wget -qO- -t1 -T2 "https://api.github.com/repos/hectorqin/reader/releases/latest" | grep "tag_name" | head -n 1 | awk -F "v" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 
 if [[ ! "${FileName}" =~ "${tag2}" ]]; then
+    rm -rf ${FileName}
     tag1=$(wget -qO- -t1 -T2 "https://api.github.com/repos/hectorqin/reader/releases/latest" | jq -r '.tag_name')
     wget https://github.com/hectorqin/reader/releases/download/${tag1}/reader-pro-${tag2}.jar
-    EXIT_CODE=$?
-    if [ ${EXIT_CODE} -eq 0 ]; then
-        rm -rf ${FileName}
-    fi
 fi
 
 if [ "${secureKey}" != "" ]; then
